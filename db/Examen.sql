0e5a194a12785f376c832dfeb69524d6daa8a955
ELVISfer <115681920+ELVISfer@users.noreply.github.com>
2023-08-18 03:11:04 -0600

    SQL

--- db/Examen.sql
+++ db/Examen.sql
@@ -4,54 +4,58 @@ create DATABASE TiendaFacturacion ;
 
 create table tblrol (
 
-    id serial PRIMARY key,
-    id_rol INTEGER PRIMARY KEY,
+    id serial NOT NULL,
+    id_rol int,
     nombre varchar(100),
     activo BOOLEAN DEFAULT true,
-    fecha_borra TIMESTAMP,
+    fecha_borra timestamp without time zone  DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY(id_rol)
 );
 
 
 create table tbl_usuario (
 
-    correo_electronico varchar(80) primary key, 
+    correo_electronico varchar(80) , 
     nombre varchar(100), 
    activo BOOLEAN DEFAULT true,
-    fecha_borra TIMESTAMP,
-    id_rol int INTEGER,
+    fecha_borra TIMESTAMP without time zone DEFAULT CURRENT_TIMESTAMP,
+    id_rol int,
 );
 
 create table tbl_continente(
-    id SERIAl PRIMARY KEY,
-    id_continente int INTEGER PRIMARY KEY,
+    id SERIAl NOT NULL,
+    id_continente int,
     nombre_continente VARCHAR(200), 
+    PRIMARY KEY(id_continente)
         );
 
 create table tbl_pais(
-    id SERIAl PRIMARY KEY,
-    id_pais int INTEGER PRIMARY KEY,
+    id SERIAl NOT null,
+    id_pais int,
     nombre_pais VARCHAR(200), 
-    id_continente int INTEGER
+    id_continente int, 
+    PRIMARY KEY(id_pais)
         );
 
 
 
 create table tbl_cuidad(
-    id SERIAl PRIMARY KEY,
-    id_cuidad int INTEGER PRIMARY KEY,
+    id SERIAl not null,
+    id_cuidad int ,
     nombre_cuidad VARCHAR(200), 
-    id_pais int INTEGER
+    id_pais int,
+    PRIMARY KEY(id_cuidad)
         );
 
 
 create table tbl_direcciones (
 
-    id serial primary key, 
+    id serial not NULL, 
     id_pais INT PRIMARY KEY
     id_cuidad INT PRIMARY KEY
     direccion varchar(50), ,
     descripcion varchar(50), 
     activo BOOLEAN DEFAULT true,
-    fecha_borra TIMESTAMP,
-    correo_electronico varchar(100) 
+    fecha_borra TIMESTAMP without time zone DEFAULT CURRENT_TIMESTAMP,
+    correo_electronico varchar(100) PRIMARY key
     );
