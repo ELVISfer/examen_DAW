dadfa2787d8d57feee5a5e00d1afa5e54f8acd6c
ELVISfer <115681920+ELVISfer@users.noreply.github.com>
2023-08-18 18:00:37 -0600

    Update Examen.sql

--- db/SQL/Examen.sql
+++ db/SQL/Examen.sql
@@ -69,3 +69,133 @@ create table tbl_direcciones (
     constraint fk_id_correo_1 Foreign Key (correo_electronico) REFERENCES tbl_usuario(id).
        constraint fk_id_pais_1 Foreign Key (id_pais) REFERENCES tbl_pais(id),
     constraint fk_id_cuidad_1 Foreign Key (id_cuidad) REFERENCES tbl_cuidad(id) );
+
+    --
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `tbl_ciudad`
+--
+ALTER TABLE tbl_ciudad
+  ADD PRIMARY KEY (id_cuidad),
+  ADD KEY id_pais (id_pais);
+
+--
+-- Indices de la tabla `tbl_continente`
+--
+ALTER TABLE tbl_continente
+  ADD PRIMARY KEY (id_continente);
+
+--
+-- Indices de la tabla `tbl_direcciones`
+--
+ALTER TABLE tbl_direcciones
+  ADD PRIMARY KEY (id_direccion),
+  ADD KEY correo_electronico (correo_electronico),
+  ADD KEY id_pais(id_pais),
+  ADD KEY id_ciudad (id_ciudad);
+
+--
+-- Indices de la tabla `tbl_pais`
+--
+ALTER TABLE tbl_pais
+  ADD PRIMARY KEY (id_pais),
+  ADD KEY id_continente (id_continente);
+
+--
+-- Indices de la tabla `tbl_rol`
+--
+ALTER TABLE tblrol
+  ADD PRIMARY KEY (id_rol);
+
+--
+-- Indices de la tabla `tbl_usuario`
+--
+ALTER TABLE tbl_usuario
+  ADD PRIMARY KEY (correo_electronico),
+  ADD KEY id_rol (id_rol);
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `tbl_ciudad`
+--
+ALTER TABLE tbl_ciudad
+  ADD CONSTRAINT id_pais FOREIGN KEY (id_pais) REFERENCES tbl_pais (id_pais);
+
+--
+-- Filtros para la tabla `tbl_direcciones`
+--
+ALTER TABLE tbl_direcciones
+  ADD CONSTRAINT correo_electronico FOREIGN KEY (correo_electronico) REFERENCES tbl_usuario (correo_electronico),
+  ADD CONSTRAINT tbl_direcciones_ibfk_1 FOREIGN KEY (id_pais) REFERENCES tbl_pais (id_pais),
+  ADD CONSTRAINT tbl_direcciones_ibfk_2 FOREIGN KEY (id_pais) REFERENCES tbl_pais (id_pais),
+  ADD CONSTRAINT tbl_direcciones_ibfk_3 FOREIGN KEY (id_ciudad) REFERENCES tbl_ciudad (id_cuidad);
+
+--
+-- Filtros para la tabla `tbl_pais`
+--
+ALTER TABLE tbl_pais
+  ADD CONSTRAINT tbl_pais_ibfk_1 FOREIGN KEY (id_continente) REFERENCES tbl_continente (id_continente);
+
+--
+-- Filtros para la tabla `tbl_usuario`
+--
+ALTER TABLE tbl_usuario
+  ADD CONSTRAINT id_rol FOREIGN KEY (id_rol) REFERENCES tblrol (id_rol);ç
+
+  
+-- Volcado de datos para la tabla `tbl_continente`
+--
+
+INSERT INTO tbl_continente (`id_continente`, `nombre_continente`) VALUES
+(1, 'Asia'),
+(2, 'America'),
+(3, 'Africa'),
+(4, 'Antartida'),
+(5, 'Europa'),
+(6, 'Oceania');
+
+--
+-- Volcado de datos para la tabla `tbl_pais`
+--
+
+INSERT INTO tbl_pais (`id_pais`, `nombre_pais`, `id_continente`) VALUES
+(1, 'Canadá', 2),
+(2, 'Estados Unidos', 2),
+(3, 'México', 2),
+(4, 'Belice', 2),
+(5, 'Costa Rica', 2),
+(6, 'El Salvador', 2),
+(7, 'Guatemala', 2),
+(8, 'Honduras', 2),
+(9, 'Nicaragua', 2),
+(10, 'Panamá', 2),
+(11, 'Argentina', 2),
+(12, 'Bolivia', 2),
+(13, 'Brasil', 2),
+(14, 'Chile', 2),
+(15, 'Colombia', 2),
+(16, 'Ecuador', 2),
+(17, 'Paraguay', 2),
+(18, 'Perú', 2),
+(19, 'Surinam', 2),
+(20, 'Trinidad y Tobago', 2),
+(21, 'Uruguay', 2),
+(22, 'Venezuela', 2),
+(23, 'Antigua y Barbuda', 2),
+(24, 'Bahamas', 2),
+(25, 'Barbados', 2),
+(26, 'Cuba', 2),
+(27, 'Dominica', 2),
+(28, 'Granada', 2),
+(29, 'Guyana', 2),
+(30, 'Haití', 2),
+(31, 'Jamaica', 2),
+(32, 'República Dominicana', 2),
+(33, 'San Cristóbal y Nieves', 2),
+(34, 'San Vicente y las Granadinas', 2),
+(35, 'Santa Lucía', 2);
